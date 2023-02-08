1aebb824f28391da3e35b82e0fa1344f41e37354
sharma39vishal <sharma39vishal@gmail.com>
2023-02-08 21:51:02 +0530

    preloader css

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -3,14 +3,7 @@ import React from 'react'
 export default function HomePage() {
   return (
     <div>
-      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam laudantium molestias hic cumque quia officia cum reprehenderit tenetur fugit. Rem, accusamus ducimus corrupti officiis magni aperiam assumenda necessitatibus blanditiis odio.</h1>
-      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam laudantium molestias hic cumque quia officia cum reprehenderit tenetur fugit. Rem, accusamus ducimus corrupti officiis magni aperiam assumenda necessitatibus blanditiis odio.</h1>
-      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam laudantium molestias hic cumque quia officia cum reprehenderit tenetur fugit. Rem, accusamus ducimus corrupti officiis magni aperiam assumenda necessitatibus blanditiis odio.</h1>
-      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam laudantium molestias hic cumque quia officia cum reprehenderit tenetur fugit. Rem, accusamus ducimus corrupti officiis magni aperiam assumenda necessitatibus blanditiis odio.</h1>
-      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam laudantium molestias hic cumque quia officia cum reprehenderit tenetur fugit. Rem, accusamus ducimus corrupti officiis magni aperiam assumenda necessitatibus blanditiis odio.</h1>
-      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam laudantium molestias hic cumque quia officia cum reprehenderit tenetur fugit. Rem, accusamus ducimus corrupti officiis magni aperiam assumenda necessitatibus blanditiis odio.</h1>
-      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam laudantium molestias hic cumque quia officia cum reprehenderit tenetur fugit. Rem, accusamus ducimus corrupti officiis magni aperiam assumenda necessitatibus blanditiis odio.</h1>
-      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam laudantium molestias hic cumque quia officia cum reprehenderit tenetur fugit. Rem, accusamus ducimus corrupti officiis magni aperiam assumenda necessitatibus blanditiis odio.</h1>
+      Homepage
     </div>
   )
 }
