a0efc4aeb8942541c0f3d88be5f47c163c44838a
sharma39vishal <sharma39vishal@gmail.com>
2023-02-08 20:28:39 +0530

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
+import Aboutus from './components/Aboutus/Aboutus';
+import Contactus from './components/ContactUs/Contactus';
+import EventsPage from './components/EventsPage/EventsPage';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import HomePage from './components/HomePage/HomePage';
+import OurTeam from './components/OurTeam/OurTeam';
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
       <Header/>
       <Routes>
           <Route exact path="/" element={<HomePage/>}/>
+          <Route exact path="/events" element={<EventsPage/>}/>
+          <Route exact path="/ourteam" element={<OurTeam/>}/>
+          <Route exact path="/aboutus" element={<Aboutus/>}/>
+          <Route exact path="/contactus" element={<Contactus/>}/>
       </Routes>
       <Footer/>
     </div>
