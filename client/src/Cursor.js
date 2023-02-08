79825dd8504451187cf6186d242051266c6474a8
Pratyushg1312 <pratyushg095@gmail.com>
2023-02-09 00:06:54 +0530

    cursor colour changed

--- client/src/Cursor.js
+++ client/src/Cursor.js
@@ -99,8 +99,8 @@ export const Cursor = () => {
       };
   return (
     <>
-    <div ref={dotOutline} className="cursor-dot-outline"></div> 
-    <div ref={dot} className="cursor-dot"></div>
+    <div ref={dotOutline} className="cursor-dot"></div> 
+    <div ref={dot} className="cursor-dot-outline"></div>
     </>
   )
 }
