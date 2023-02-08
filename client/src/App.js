82ee9a57f04faa9efba246f5748bd9fc64c73c7f
sharma39vishal <sharma39vishal@gmail.com>
2023-02-08 21:17:46 +0530

    preloader

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes } from "react-router-dom";
 import Aboutus from './components/Aboutus/Aboutus';
 import Contactus from './components/ContactUs/Contactus';
@@ -7,10 +7,21 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import HomePage from './components/HomePage/HomePage';
 import OurTeam from './components/OurTeam/OurTeam';
+import Preloader from './components/Preloader/Preloader';
 
 function App() {
+  const [loading, setloading] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {setloading(true)}, 3500);
+    // window.onload=()=>{
+    //   setloading(true);
+    //   }
+  },[]);  
+
   return (
     <div>
+      {loading?
+      <div>
       <Header/>
       <Routes>
           <Route exact path="/" element={<HomePage/>}/>
@@ -20,6 +31,8 @@ function App() {
           <Route exact path="/contactus" element={<Contactus/>}/>
       </Routes>
       <Footer/>
+      </div>
+      :<Preloader/>}
     </div>
   );
 }
