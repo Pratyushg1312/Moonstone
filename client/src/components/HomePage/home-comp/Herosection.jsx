06d1b3baf20c24fcb9dff5fae50cc7e933adcae4
Pratyushg1312 <pratyushg095@gmail.com>
2023-02-12 18:05:35 +0530

    added animation on loading

--- client/src/components/HomePage/home-comp/Herosection.jsx
+++ client/src/components/HomePage/home-comp/Herosection.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import './herosec.css'
 import moon from "./images/moon.png"
 export const Herosection = () => {
-  
+
   return (
     <div className="Herosection container">
-        <div className="hscon">
-        <img  className="moonimg" src={moon} alt="" />
-        <h1>MOON STONE</h1>
+      <div className="hscon">
+        <div className="mi1">
+          <img className="moonimg" src={moon} alt="" />
         </div>
+        
+          <h1>MOON STONE</h1>
+        
+      </div>
     </div>
   )
 }
