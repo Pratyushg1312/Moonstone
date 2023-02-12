0f63b1d6e15e48bc2630ff5ead355874af41ff6f
sharma39vishal <sharma39vishal@gmail.com>
2023-02-12 16:37:54 +0530

    nav bar

--- client/src/components/HomePage/home-comp/PastEvents.js
+++ client/src/components/HomePage/home-comp/PastEvents.js
@@ -1,24 +1,69 @@
-import React from 'react'
-import PastEventComponent from './PastEventComponent'
-import {Data} from './PreventComponentData';
 import './PastEvents.css'
-export default function PastEvents() {
-    return (
-        <>
-            <div className='container text-center my-5' style={{ color: "white" }}>
-                <div>
-                    <h1 >Past Events</h1>
-                    <p style={{ textDecoration: "underline" }}>Glimpses of Our Events</p>
-                </div>
-                <div className='row'>
-                    <div className="gallery-image ">
-                        {Data.map((elem)=>{
 
-                            return <PastEventComponent name={elem.name} desc={elem.desc} />
-                        })}
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+import React, { useEffect } from 'react'
+
+export default function PastEvents() {
+
+  useEffect(() => {
+    (function () {
+      // "use strict";    
+      var items = document.querySelectorAll(".timeline li");
+      function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <=
+            (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+      }
+    
+      function callbackFunc() {
+        for (var i = 0; i < items.length; i++) {
+          if (isElementInViewport(items[i])) {
+            items[i].classList.add("in-view");
+          }
+        }
+      }
+    
+      // listen for events
+      window.addEventListener("load", callbackFunc);
+      window.addEventListener("resize", callbackFunc);
+      window.addEventListener("scroll", callbackFunc);
+    })();
+        
+  
+    
+  }, [])
+  
+
+  return (
+    <div><section class="intro">
+  <div class="container">
+    <h1>Past Events </h1>
+  </div>
+</section>
+<section class="timeline">
+  <ul>
+    <li>
+      <div>
+        <time>1934</time> At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+      </div>
+    </li>
+    <li>
+      <div>
+        <time>1937</time> Proin quam velit, efficitur vel neque vitae, rhoncus commodo mi. Suspendisse finibus mauris et bibendum molestie. Aenean ex augue, varius et pulvinar in, pretium non nisi.
+      </div>
+    </li>
+    <li>
+      <div>
+        <time>1940</time> Proin iaculis, nibh eget efficitur varius, libero tellus porta dolor, at pulvinar tortor ex eget ligula. Integer eu dapibus arcu, sit amet sollicitudin eros.
+      </div>
+    </li>
+  </ul>
+</section>
+
+</div>
+  )
 }
