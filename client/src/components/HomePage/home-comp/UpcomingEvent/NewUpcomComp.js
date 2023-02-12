daf3d1ffe7dbfe810e186b2d8bd8e142f4efa3ba
Kalash Jain <kalashjain446@gmail.com>
2023-02-12 18:42:36 +0530

    upcoming eventadded

---
+++ client/src/components/HomePage/home-comp/UpcomingEvent/NewUpcomComp.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Upcom.css'
+export default function NewUpcomComp() {
+    return (
+        <div>
+            <div className="cardBox"> 
+      
+                <div className="card" style={{height:"480px", background: "#000814"}}>
+                    <h2>Event image</h2>
+                    <span>Hover Me</span>
+                    <div className="content">
+                        <h3>event name</h3>
+                        <p>event description</p>
+                    </div>
+                </div>
+            </div>
+          
+        </div>
+    )
+}
--- client/src/components/HomePage/home-comp/UpcomingEvent/UpcomingEvent.js
+++ client/src/components/HomePage/home-comp/UpcomingEvent/UpcomingEvent.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import UpcomingEventComp from './UpcomingEventComp'
 import { Data } from './Data'
+import NewUpcomComp from './NewUpcomComp'
 export default function UpcomingEvent() {
   return (
     <div className='my-2'>
       <h1>Upcoming Events</h1>
-      <div className='d-flex row text-center justify-content-center align-items-center'>
+      <div className='row d-flex text-center justify-content-center'>
+
+
         {
-            Data.map((elem,key)=>{
-            return <UpcomingEventComp idx={key}/>
-            })
+          Data.map((elem, key) => {
+            return <div className=' d-flex col-lg-4 col-10  justify-content-center  text-center'>
+              <NewUpcomComp />
+            </div>
+          })
         }
       </div>
+
     </div>
   )
 }
